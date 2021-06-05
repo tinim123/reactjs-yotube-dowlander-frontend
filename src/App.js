aac9f39799eaa858bd14fd8da54b6d8b41291614
tinim123 <hasan_melpa@hotmail.com>
2021-06-05 07:47:13 +0300

    frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import YoutubeVideo from "./YotubeVideo";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [url, setUrl] = useState("");
+  const [youtube, setYoutube] = useState(false);
+  const [loading, setLoading] = useState(false)
+
+  const download = () => {
+    if (url) {
+      setLoading(true)
+      fetch(`https://tinim123.pythonanywhere.com/api/youtube?url=${url}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setLoading(false)
+          setYoutube(data)
+        });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="" method="post" onSubmit={(e) => e.preventDefault()}>
+        <h3>Youtube Downloader</h3>
+        <div className="search">
+          <input
+            type="text"
+            onChange={(e) => setUrl(e.target.value)}
+            placeholder="Youtube URL"
+          />
+          <button type="submit" onClick={download}>
+            Download
+          </button>
+        </div>
+      </form>
+      {loading && <div className="loader">Yükleniyor..</div>}
+      {youtube && loading===false && <YoutubeVideo youtube={youtube} />}
     </div>
   );
 }
